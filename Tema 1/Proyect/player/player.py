ac07af9b2875e2e12666893fced22183caaa6a67
angelgarciaguillena <angel.garcia@iesnervion.es>
2025-10-26 20:14:48 +0100

    Proyecto realizado

---
+++ Tema 1/Proyect/player/player.py
@@ -0,0 +1,216 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+#Creamos una apicacion FastAPI  
+app = FastAPI()
+
+#Creamos la clase Jugador
+class Player(BaseModel):
+    id: int #Id del jugador
+    name: str #Nombre del jugador
+    age: int #Edad del jugador
+    position: str #Posicion del jugador
+    nationality: str #Nacionalidad del jugador
+    salary: float #Salario del jugador
+    id_team: int #Id del equipo al que pertenece el jugador
+
+#Lista de jugadores de ejemplo
+player_list = [
+    Player(id=1, name="Isco", age=31, position="Midfielder", nationality="Spanish", salary=350000, id_team=1),
+    Player(id=2, name="Robert Lewandowski", age=35, position="Forward", nationality="Polish", salary=800000, id_team=2),
+    Player(id=3, name="Jude Bellingham", age=20, position="Midfielder", nationality="English", salary=750000, id_team=3),
+    Player(id=4, name="Antoine Griezmann", age=32, position="Forward", nationality="French", salary=600000, id_team=4),
+    Player(id=5, name="Sergio Ramos", age=37, position="Defender", nationality="Spanish", salary=450000, id_team=5),
+    Player(id=6, name="José Gayà", age=28, position="Defender", nationality="Spanish", salary=300000, id_team=6),
+    Player(id=7, name="Gerard Moreno", age=31, position="Forward", nationality="Spanish", salary=350000, id_team=7),
+    Player(id=8, name="Iñaki Williams", age=29, position="Forward", nationality="Ghanaian", salary=300000, id_team=8),
+    Player(id=9, name="Take Kubo", age=22, position="Forward", nationality="Japanese", salary=250000, id_team=9),
+    Player(id=10, name="Iago Aspas", age=36, position="Forward", nationality="Spanish", salary=400000, id_team=10)
+]
+
+
+#Funcion para obtener el siguiente ID disponible
+def next_id():
+    return max(player_list, key = id).id + 1
+
+#region Metodos get
+#Metodo get para obtener todos los jugadores
+@app.get("/players")
+def get_players():
+    return player_list
+
+
+#Metodo get para obtener el jugador por su id
+@app.get("/players/{id_player}")
+def get_player_by_id(id_player: int):
+
+    #Buscamos el jugador por su ID y lo almacenamos en una lista
+    players = [player for player in player_list if player.id == id_player]
+
+    #Si encontramos al jugadorlo devolvemos
+    if players:
+        return players[0]
+
+    #Si no encontramos al jugador devolvemos un mensaje de error
+    else:
+        return {"error": "Player not found by ID"}
+
+
+#Metodo get para obtener los jugadores por su nombre
+@app.get("/players/name/{player_name}")
+def get_player_by_name(player_name: str):
+
+    #Buscamos los jugadores por su nombre y los almacenamos en una lista
+    players = [player for player in player_list if player.name.lower() == player_name.lower()] 
+
+    #Si encontramos a los jugadores los devolvemos
+    if players:
+        return players
+    
+    #Si no encontramos al jugador devolvemos un mensaje de error
+    else:
+        return {"error": "Players not found by name"}
+
+
+#Metodo get para obtener los jugadores por su edad
+@app.get("/players/age/{player_age}")
+def get_player_by_age(player_age: int):
+
+    #Buscamos los jugadores por su edad y los almacenamos en una lista
+    players = [player for player in player_list if player.age == player_age]
+
+    #Si encontramos a los jugadores los devolvemos
+    if players:
+        return players
+    
+    #Si no encontramos al jugador devolvemos un mensaje de error
+    else:
+        return {"error": "Players not found by age"}
+
+
+#Metodo get para obtener los jugadores por su posicion
+@app.get("/players/position/{player_position}")
+def get_player_by_position(player_position: str):
+
+    #Buscamos los jugadores por su posicion y los almacenamos en una lista
+    players = [player for player in player_list if player.position.lower() == player_position.lower()]   
+
+    #Si encontramos a los jugadores los devolvemos
+    if players:
+        return players
+    
+    #Si no encontramos al jugador devolvemos un mensaje de error
+    else:
+        return {"error": "Players not found by position"}
+
+
+#Metodo get para obtener los jugadores por su nacionalidad
+@app.get("/players/nationality/{player_nationality}")
+def get_player_by_nationality(player_nationality: str):
+
+    #Buscamos los jugadores por su nacionalidad y los almacenamos en una lista
+    players = [player for player in player_list if player.nationality.lower() == player_nationality.lower()]
+
+    #Si encontramos a los jugadores los devolvemos
+    if players:
+        return players
+    
+    #Si no encontramos al jugador devolvemos un mensaje de error
+    else:
+        return {"error": "Players not found by nationality"}
+
+
+#Metodo get para obtener los jugadores por su salario 
+@app.get("/players/salary/{player_salary}")
+def get_player_by_salary(player_salary: float):
+
+    #Buscamos los jugadores por su salario y los almacenamos en una lista
+    players = [player for player in player_list if player.salary == player_salary]
+
+    #Si encontramos a los jugadores los devolvemos
+    if players:
+        return players
+    
+    #Si no encontramos al jugador devolvemos un mensaje de error
+    else:
+        return {"error": "Players not found by salary"}
+
+
+#Metodo get para obtener los jugadores por su ID de equipo 
+@app.get("/players/id_team/{id_team}")
+def get_player_by_team(id_team: int):
+
+    #Buscamos los jugadores por su ID de equipo y los almacenamos en una lista
+    players = [player for player in player_list if player.id_team == id_team]
+
+    #Si encontramos a los jugadores los devolvemos
+    if players:
+        return players
+    
+    #Si no encontramos al jugador devolvemos un mensaje de error
+    else:
+        return {"error": "Players not found by team ID"}
+#endregion
+
+
+#region Metodos post
+#Metodo post para añadir un nuevo jugador
+@app.post("/players")
+def add_player(player: Player):
+
+    #LLamamos a la funcion para asignar un ID al nuevo jugador 
+    player.id = next_id()
+
+    #Añadimos el nuevo jugador a la lista de jugadores
+    player_list.append(player)
+
+    #Devolvemos el jugador que se ha añadido
+    return player
+#endregion
+
+
+#region Metodos put
+#Metodo put para modificar un jugador existente
+@app.put("/players/{id_player}", response_model = Player)
+def modify_player(id_player: int, player: Player):
+
+    #Creamos un bucle for para recorrer la lista de jugadores
+    for index, saved_player in enumerate(player_list):
+
+        #Si el id del jugador es igual al id que queremos modificar, modificamos el jugador
+        if saved_player.id == id_player:
+
+            #Asignamos el ID al jugador modificado
+            player.id = id_player
+
+            #Actualizamos el jugador en la lista de jugadores
+            player_list[index] = player
+
+            #Devolvemos el jugador que se ha modificado
+            return player
+
+    #Si no encontramos al jugador devolvemos un mensaje de error
+    raise HTTPException(status_code = 404, detail = "Player not found")
+#endregion
+
+
+#region Metodos delete
+#Metodo delete para eliminar un jugador existente
+@app.delete("/players/{id_player}")
+def delete_player(id_player: int):
+
+    #Creamos un bucle for para recorrer la lista de jugadores
+    for saved_player in player_list:
+
+        #Si el id del jugador es igual al id que queremos eliminar, eliminamos el jugador
+        if saved_player.id == id_player:
+
+            #Eliminamos el jugador de la lista de jugadores
+            player_list.remove(saved_player)
+
+            #Devolvemos un diccionario vacio
+            return {}
+    
+    #Si no encontramos al jugador devolvemos un mensaje de error
+    raise HTTPException(status_code = 404, detail = "Player not found")
+#endregion
--- Tema 1/Proyect/team/team.py
+++ Tema 1/Proyect/team/team.py
@@ -6,11 +6,12 @@ app = FastAPI()
 
 #Clase Equipo
 class Team(BaseModel):
-    id: int
-    name: str
-    city: str
-    year_founded: int
-    stadium: str
+    id: int #Id del equipo
+    name: str #Nombre del equipo
+    city: str #Ciudad del equipo
+    year_founded: int #Año de fundacion del equipo
+    stadium: str #Estadio del equipo
+
 
 #Lista de equipos de ejemplo
 team_list = [
@@ -26,9 +27,13 @@ team_list = [
     Team(id=10, name="Celta de Vigo", city="Vigo", year_founded=1923, stadium="Balaídos"),
 ]
 
+
+#Funcion para obtener el siguiente ID disponible
 def next_id():
     return (max(team_list, key = id).id+1)
 
+
+#region Metodos get
 #Metodo get para obtener todos los equipos
 @app.get("/teams")   
 def teams():
@@ -38,11 +43,15 @@ def teams():
 #Metodo get para obtener el equipo por su ID
 @app.get("/teams/{id_team}")
 def get_team_by_id(id_team: int):
-    
+
+    #Buscamos el equipo por su ID y lo almacenamos en una lista
     teams = [team for team in team_list if team.id == id_team]
 
-    if len(teams) != 0:
+    #Si encontramos al equipo lo devolvemos
+    if teams:
         return teams[0]
+    
+    #Si no encontramos al equipo devolvemos un mensaje de error
     else:
         return {"error": "Team not found by ID"}
 
@@ -51,10 +60,14 @@ def get_team_by_id(id_team: int):
 @app.get("/teams/name/{team_name}")
 def get_team_by_name(team_name: str):
 
+    #Buscamos los equipos por su nombre y los almacenamos en una lista
     teams = [team for team in team_list if team.name.lower() == team_name.lower()]
 
-    if len(teams) != 0:
+    #Si encontramos los equipos los devolvemos
+    if teams:
         return teams
+
+    #Si no encontramos los equipos devolvemos un mensaje de error
     else:
         return {"error": "Teams not found by name"}
 
@@ -63,10 +76,14 @@ def get_team_by_name(team_name: str):
 @app.get("/teams/city/{city_name}")
 def get_teams_by_city(city_name: str):
 
+    #Buscamos los equipos por su ciudad y los almacenamos en una lista
     teams = [team for team in team_list if team.city.lower() == city_name.lower()]
-    
-    if len(teams) != 0:
+
+    #Si encontramos los equipos los devolvemos
+    if teams:
         return teams
+    
+    #Si no encontramos los equipos devolvemos un mensaje de error
     else:
         return {"error": "Teams not found by city name"}
 
@@ -75,10 +92,14 @@ def get_teams_by_city(city_name: str):
 @app.get("/teams/year/{year_founded}")
 def get_teams_by_year(year_founded: int):
 
+    #Buscamos los equipos por su año de fundacion y los almacenamos en una lista
     teams = [team for team in team_list if team.year_founded == year_founded]
 
-    if len(teams) != 0:
+    #Si encontramos los equipos los devolvemos
+    if teams:
         return teams
+    
+    #Si no encontramos los equipos devolvemos un mensaje de error
     else:
         return {"error": "Teams not found by year of foundation"}
 
@@ -87,78 +108,74 @@ def get_teams_by_year(year_founded: int):
 @app.get("/teams/stadium/{stadium_name}")
 def get_teams_by_stadium(stadium_name: str):
 
+    #Buscamos los equipos por su estadio y los almacenamos en una lista
     teams = [team for team in team_list if team.stadium.lower() == stadium_name.lower()]
-    
-    if len(teams) != 0:
+
+    #Si encontramos los equipos los devolvemos
+    if teams:
         return teams
+    
+    #Si no encontramos los equipos devolvemos un mensaje de error
     else:
         return {"error": "Teams not found by stadium name"}
+#endregion
 
+#region Metodos post
 #Metodo post para añadir un nuevo equipo
 @app.post("/teams", status_code = 201, response_model = Team)
 def add_team(team : Team):
 
+    #Llamamos a la funcion para asignar un ID al nuevo equipo
     team.id = next_id()
 
+    #Añadimos el nuevo equipo a la lista de equipos
     team_list.append(team)
 
+    #Devolvemos el equipo que se ha añadido
     return team
+#endregion
 
+#region Metodos put
+#Metodo put para modificar un equipo existente
 @app.put("/teams/{id_team}", response_model = Team)
-def modify_team_id(id: int, team: Team):
-    for index, saved_team in enumerate(team_list):
-        if saved_team.id == id:
-            team.id = id
-            team_list[index] = team
-            return team
-        
-    raise HTTPException(status_code = 404, detail = "Team not found")
+def modify_team(id_team: int, team: Team):
 
-@app.put ("/teams/name/{team_name}", response_model = Team)
-def modify_team_name(id: int, name: str, team = Team):
+    #Creamos un bucle for para recorrer la lista de equipos
     for index, saved_team in enumerate(team_list):
-        if saved_team.id == id:
-            team.name = name
-            team_list[index] = team
-            return team
-        
-    raise HTTPException(status_code = 404, detail = "Team not found")
 
-@app.put ("/teams/city/{city_name}", response_model = Team)
-def modify_team_city(id: int, city: str, team = Team):
-    for index, saved_team in enumerate(team_list):
-        if saved_team.id == id:
-            team.city = city
-            team_list[index] = team
-            return team
-        
-    raise HTTPException(status_code = 404, detail = "Team not found")
+        #Si el id del equipo es igual al id que queremos modificar, modificamos el equipo
+        if saved_team.id == id_team:
 
-@app.put ("/teams/year/{year_founded}", response_model = Team)
-def modify_team_year(id: int, year_founded: int, team = Team):
-    for index, saved_team in enumerate(team_list):
-        if saved_team.id == id:
-            team.year_founded == year_founded
-            team_list[index] = team
-            return team
-        
-    raise HTTPException(status_code = 404, detail = "Team not found")
+            #Asignamos el ID al equipo modificado
+            team.id = id_team
 
-@app.put ("/teams/stadium/{stadium_name}", response_model = Team)
-def modify_team_stadium(id: int, stadium: str, team = Team):
-    for index, saved_team in enumerate(team_list):
-        if saved_team.id == id:
-            team.stadium == stadium
+            #Actualizamos el equipo en la lista de equipos
             team_list[index] = team
-            return team
-    
-    raise HTTPException(status_code = 404, detail = "Team not found")
 
-@app.delete("teams/{id_team}")
-def delete_team(id: int):
+            #Devolvemos el equipo que se ha modificado
+            return team
+
+    #Si no encontramos el equipo devolvemos un mensaje de error 
+    raise HTTPException(status_code = 404, detail = "Team not found")
+#endregion
+
+#region Metodos delete
+#Metodo delete para eliminar un equipo existente
+@app.delete("/teams/{id_team}")
+def delete_team(id_team: int):
+
+    #Creamos un bucle for para recorrer la lista de equipos
     for saved_team in team_list:
-        if saved_team.id == id:
+
+        #Si el id del equipo es igual al id que queremos eliminar, eliminamos el equipo
+        if saved_team.id == id_team:
+
+            #Eliminamos el equipo de la lista de equipos
             team_list.remove(saved_team)
+
+            #Devolvemos un diccionario vacio
             return {}
-        
+
+    #Si no encontramos el equipo devolvemos un mensaje de error
     raise HTTPException(status_code = 404, detail = "Team not found")
+#endregion
