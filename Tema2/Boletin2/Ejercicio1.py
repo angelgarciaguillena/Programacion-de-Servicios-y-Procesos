5cf69d98d85c81f93e8dedc216d5bdb6c393b342
angelgarciaguillena <angel.garcia@iesnervion.es>
2026-01-21 10:25:22 +0100

    Cambios

---
+++ Tema2/Boletin2/Ejercicio1.py
@@ -0,0 +1,14 @@
+from multiprocessing import Process, Pipe
+import os
+import time
+
+def contarVocales():
+    
+    
+def leerFichero():
+    
+if __name__ == "__main__":
+    
+    inicio = time.time()
+    
+    fichero = 'C:\\Users\\angel.garcia\\Documents\\Programacion-de-Servicios-y-Procesos\\Tema2\\Boletin2\\vocales.txt'
