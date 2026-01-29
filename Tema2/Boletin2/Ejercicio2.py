21d8cef6f6aeca0f68a76b1ba80c55fc5a50e65a
angelgarciaguillena <angel.garcia@iesnervion.es>
2026-01-29 23:57:03 +0100

    Ejercicios 2-3 realizados

--- Tema2/Boletin2/Ejercicio2.py
+++ Tema2/Boletin2/Ejercicio2.py
@@ -2,42 +2,81 @@ from multiprocessing import Process, Pipe
 import time
 import random
 
-def generarDireccion():
+def generarDireccion(conexion):
     
-    direcciones = []
     ip = ""
     
     for i in range(10):
         
         for j in range(4):
-            secuencia = random.nextInt(0, 255)
+            secuencia = random.randint(0, 255)
             
             ip += str(secuencia)
             
             if j < 3:
                 ip += "."
                 
-        direcciones.append(ip)
+        conexion.send(ip)
         ip = ""
         
-    return direcciones   
+    conexion.send(None)
+    conexion.close()
     
     
-def clasificarDireccion():
+def clasificarDireccion(entrada, salida):
+
+    direccion = entrada.recv()
+    contador = 0
+
+    print("\nClasificación de IPs:")
+    
+    while direccion is not None:
+        secuencia = int(direccion.split('.')[0])
+        
+        if (1 <= secuencia <= 126) or (128 <= secuencia <= 191) or (192 <= secuencia <= 223):
+            print(f"IP válida: {direccion}")
+            salida.send(direccion)
+            contador += 1
+        else:
+            print(f"IP descartada: {direccion}")
+        
+        direccion = entrada.recv()
+    
+    salida.send(None)
+    entrada.close()
+    salida.close()
     
     
-def mostrarDireccion():
+def mostrarDireccion(conexion):
+    direccion = conexion.recv()
+
+    print("\nLista de IPs:")
+
+    while direccion is not None:
+        secuencia = int(direccion.split('.')[0])
+        
+        if 1 <= secuencia <= 126:
+            clase = "A"
+        elif 128 <= secuencia <= 191:
+            clase = "B"
+        elif 192 <= secuencia <= 223:
+            clase = "C"
+        
+        print(f"{direccion} - Clase {clase}")
+        
+        direccion = conexion.recv()
     
+    conexion.close()
+    
+
 if __name__ == "__main__":
     
     inicio = time.time()
 
-    fichero = 'C:\\Users\\Usuario\\Documents\\Programacion-de-Servicios-y-Procesos\\Tema2\\Boletin1\\numeros.txt'
-
     left, right = Pipe()
     left2, right2 = Pipe()
 
-    p1 = Process(target = generarDireccion, args = (fichero, left))
+    p1 = Process(target = generarDireccion, args = (left,))
     p2 = Process(target = clasificarDireccion, args = (right, left2))
     p3 = Process(target = mostrarDireccion, args = (right2,))
     
---
+++ Tema2/Boletin2/Ejercicio3.py
@@ -0,0 +1,85 @@
+from multiprocessing import Lock, Pool
+import random
+import time
+
+lock = Lock()
+
+def generarNumero(fichero):
+
+    f = open(fichero, 'w')
+    notas = []
+    
+    with open(fichero, 'w') as f:
+        for i in range(6):
+            numero = random.randint(1, 10)
+            f.write(f"{numero}\n")
+            notas.append(numero)
+
+    f.close()
+    return notas
+
+
+def calcularMedia(ficheroLectura, ficheroEscritura, alumno):
+
+    f1 = open(ficheroLectura, 'r')
+    suma = 0
+    contador = 0
+
+    with open(ficheroLectura, 'r') as f1:
+        for linea in f1.readlines():
+            suma += int(linea.strip())
+            contador += 1
+
+    media = suma / contador
+
+    f1.close()
+    with lock:
+        with open(ficheroEscritura, 'w') as f2:
+            f2.write(f"{alumno} {media}\n")
+
+    return (alumno, media)
+
+
+def obtenerNotaMaxima(fichero):
+
+    f = open(fichero, 'r')
+
+    max_nota = -1
+    alumno_max = ""
+
+    with open(fichero, 'r') as f:
+        for linea in f.readlines():
+            alumno, nota = linea.strip().split(" ")
+            nota = float(nota)
+            if nota > max_nota:
+                max_nota = nota
+                alumno_max = alumno
+
+    f.close()
+    return (alumno_max, max_nota)
+
+
+if __name__ == '__main__':
+
+    inicio = time.time()
+    alumnos = [f"Alumno{i+1}" for i in range(10)]
+    ficheros_notas = [f"C:\\Users\\Usuario\\Documents\\Programacion-de-Servicios-y-Procesos\\Tema2\\Boletin2\\Alumno{i+1}.txt" for i in range(10)]
+    fichero_medias = 'medias.txt'
+
+    with Pool(processes=len(alumnos)) as pool:
+        pool.map(generarNumero, ficheros_notas)
+
+    args = list(zip(ficheros_notas, [fichero_medias]*len(alumnos), alumnos))
+    with Pool(processes=len(alumnos)) as pool:
+        pool.starmap(calcularMedia, args)
+
+
+    with Pool(processes=1) as pool:
+        resultados = pool.map(obtenerNotaMaxima, (fichero_medias,))
+
+    alumno_max, max_nota = resultados[0]
+    
+    print(f"Alumno con la nota máxima: {alumno_max} con una nota de {max_nota}")
+
+    fin = time.time()
+    print(f"Tiempo total de ejecución: {fin - inicio} segundos")
